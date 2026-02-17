8958e810c44f68cf985b771313392959b7121006
nhan.dev <hey@nhan.dev>
2026-02-17 18:41:30 +0700

    Smooth Scroll

---
+++ src/src/scripts/gsap.ts
@@ -0,0 +1,15 @@
+import { gsap } from 'gsap'
+import { ScrollSmoother } from 'gsap/ScrollSmoother'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { SplitText } from 'gsap/SplitText'
+
+gsap.registerPlugin(ScrollSmoother, ScrollTrigger, SplitText)
+
+export const smoother = ScrollSmoother.create({
+  wrapper: 'main',
+  content: '.smooth-content',
+  smooth: 1,
+  effects: true,
+  normalizeScroll: true,
+  ignoreMobileResize: true,
+})
